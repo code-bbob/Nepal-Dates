79d401606443806509ffb8f1c154b97a2850bea6
grish <grishgautam17@gmail.com>
2024-02-09 11:34:56 -0600

    class names

--- backend/profiles/models.py
+++ backend/profiles/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model):
+    description = models.CharField(max_length = 100)
+    Sex = models.BooleanField()
+    age = models.IntegerField()
+    user = models.ForeignKey(User)
+    image = models.ImageField(upload_to="profiles/images")
+    
